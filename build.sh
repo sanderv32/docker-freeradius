0e996f0d3cecc20ef9e0bfda1f47a5c0fec501f8
Alexander Verhaar <sanderv32@gmail.com>
2015-02-28 23:51:31 +0100

    Show radius secret at the end of the build process

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ docker rm -f freeradius
 docker rmi sanderv32/freeradius
 
 RADIUS_SECRET=$(openssl rand -base64 24)
-echo $RADIUS_SECRET
 cat Dockerfile.template | sed "s/%RADIUS_SECRET%/$RADIUS_SECRET/g" > Dockerfile
 
 docker build -t sanderv32/freeradius .
+echo "--------------------------------------------------"
+echo "Radius secret: $RADIUS_SECRET"
+echo "--------------------------------------------------"
