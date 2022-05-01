5e40143a4f6716f1d4b3a1179322b09b0dfb495e
Alexander Verhaar <sanderv32@gmail.com>
2022-05-01 15:47:11 +0200

    Some changes still not pushed

--- start-docker.sh
+++ start-docker.sh
@@ -3,4 +3,12 @@
 NAME="freeradius"
 HOSTNAME="freeradius"
 
-docker run -p 1812:1812/udp -h $HOSTNAME --name $NAME -d sanderv32/freeradius
+docker run \
+	--restart=always \
+	-p 1812:1812/udp \
+	-p 1813:1813/udp \
+	-p 1814:1814/udp \
+	-p 18120:18120/udp \
+	-h $HOSTNAME \
+	--name $NAME \
+	-d sanderv32/freeradius
